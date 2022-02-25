78445762b078b2cd3637737d049953f8774cc91d
Shashwat Nautiyal <shashwatnautiyal2015@gmail.com>
2022-02-26 00:55:56 +0530

    Animate bg updated

--- app.js
+++ app.js
@@ -13,64 +13,64 @@ const animateBackground = () => {
 
 	function toColorPlus() {
 		if (r == 255 && g == 0 && b < 255) {
-			b++;
+			b += 5;
 		}
 		if (g == 0 && b == 255 && r > 0) {
-			r--;
+			r -= 5;
 		}
 		if (r == 0 && b == 255 && g < 255) {
-			g++;
+			g += 5;
 		}
 		if (r == 0 && g == 255 && b > 0) {
-			b--;
+			b -= 5;
 		}
 		if (g == 255 && b == 0 && r < 255) {
-			r++;
+			r += 5;
 		}
 		if (r === 255 && b == 0 && g > 0) {
-			g--;
+			g -= 5;
 		}
 	}
 
 	function toColorPlus1() {
 		if (r1 == 255 && g1 == 0 && b1 < 255) {
-			b1++;
+			b1 += 5;
 		}
 		if (g1 == 0 && b1 == 255 && r1 > 0) {
-			r1--;
+			r1 -= 5;
 		}
 		if (r1 == 0 && b1 == 255 && g1 < 255) {
-			g1++;
+			g1 += 5;
 		}
 		if (r1 == 0 && g1 == 255 && b1 > 0) {
-			b1--;
+			b1 -= 5;
 		}
 		if (g1 == 255 && b1 == 0 && r1 < 255) {
-			r1++;
+			r1 += 5;
 		}
 		if (r1 === 255 && b1 == 0 && g1 > 0) {
-			g1--;
+			g1 -= 5;
 		}
 	}
 
 	function toColorPlus2() {
 		if (r2 == 255 && g2 == 0 && b2 < 255) {
-			b2++;
+			b2 += 5;
 		}
 		if (g2 == 0 && b2 == 255 && r2 > 0) {
-			r2--;
+			r2 -= 5;
 		}
 		if (r2 == 0 && b2 == 255 && g2 < 255) {
-			g2++;
+			g2 += 5;
 		}
 		if (r2 == 0 && g2 == 255 && b2 > 0) {
-			b2--;
+			b2 -= 5;
 		}
 		if (g2 == 255 && b2 == 0 && r2 < 255) {
-			r2++;
+			r2 += 5;
 		}
 		if (r2 === 255 && b2 == 0 && g2 > 0) {
-			g2--;
+			g2 -= 5;
 		}
 	}
 
@@ -80,113 +80,113 @@ const animateBackground = () => {
 		toColorPlus2();
 
 		document.getElementsByTagName(
-			'BODY'
+			"BODY"
 		)[0].style.background = `linear-gradient(60deg, rgb(${r},${g},${b}) ,rgb(${r1},${g1},${b1}) ,rgb(${r2},${g2},${b2}))`;
-	}, 100);
+	}, 250);
 };
 
 // Variables
 
-const inputBox = document.querySelector('.input-box');
-const inputBoxEdit = document.querySelector('.input-box-edit');
-const taskName = document.querySelector('.task-name');
-const taskDesc = document.querySelector('.task-desc');
-const taskList = document.querySelector('.task-list');
-const editBtn = document.querySelector('.edit-task');
-const editTaskName = document.querySelector('.edit-task-name');
-const editTaskDesc = document.querySelector('.edit-task-desc');
+const inputBox = document.querySelector(".input-box");
+const inputBoxEdit = document.querySelector(".input-box-edit");
+const taskName = document.querySelector(".task-name");
+const taskDesc = document.querySelector(".task-desc");
+const taskList = document.querySelector(".task-list");
+const editBtn = document.querySelector(".edit-task");
+const editTaskName = document.querySelector(".edit-task-name");
+const editTaskDesc = document.querySelector(".edit-task-desc");
 
 const toDoApp = () => {
 	// Selecting HTML Elements
-	const addBtn = document.querySelector('.add-btn');
-	const addTaskBtn = document.querySelector('.add-task');
-	const cancelTaskBtn = document.querySelector('.cancel-task');
-	const cancelEditTaskBtn = document.querySelector('.cancel-edit-task');
+	const addBtn = document.querySelector(".add-btn");
+	const addTaskBtn = document.querySelector(".add-task");
+	const cancelTaskBtn = document.querySelector(".cancel-task");
+	const cancelEditTaskBtn = document.querySelector(".cancel-edit-task");
 
-	addBtn.addEventListener('click', openTaskFrom);
-	taskList.addEventListener('click', delCheckTask);
-	addTaskBtn.addEventListener('click', addTask);
-	cancelTaskBtn.addEventListener('click', cancelTask);
-	cancelEditTaskBtn.addEventListener('click', cancelEditTask);
+	addBtn.addEventListener("click", openTaskFrom);
+	taskList.addEventListener("click", delCheckTask);
+	addTaskBtn.addEventListener("click", addTask);
+	cancelTaskBtn.addEventListener("click", cancelTask);
+	cancelEditTaskBtn.addEventListener("click", cancelEditTask);
 
 	animateBackground();
 };
 
 const openTaskFrom = () => {
-	inputBox.classList.add('show');
-	taskList.style.filter = 'blur(10px)';
+	inputBox.classList.add("show");
+	taskList.style.filter = "blur(10px)";
 };
 
 const addTask = (e) => {
 	createTask();
-	inputBox.classList.remove('show');
-	taskList.style.filter = 'blur(0)';
+	inputBox.classList.remove("show");
+	taskList.style.filter = "blur(0)";
 };
 
 const cancelTask = (e) => {
-	inputBox.classList.remove('show');
-	taskList.style.filter = 'blur(0)';
+	inputBox.classList.remove("show");
+	taskList.style.filter = "blur(0)";
 };
 
 const cancelEditTask = (e) => {
-	inputBoxEdit.classList.remove('show');
-	taskList.style.filter = 'blur(0)';
+	inputBoxEdit.classList.remove("show");
+	taskList.style.filter = "blur(0)";
 };
 
 const editTask = (e) => {
 	editTaskName.value = e.target.parentElement.parentElement.children[0].children[0].innerText;
 	editTaskDesc.value = e.target.parentElement.parentElement.children[0].children[1].innerText;
-	editBtn.addEventListener('click', () => {
+	editBtn.addEventListener("click", () => {
 		e.target.parentElement.parentElement.children[0].children[0].innerText = editTaskName.value;
 		e.target.parentElement.parentElement.children[0].children[1].innerText = editTaskDesc.value;
 		cancelEditTask();
 	});
 
-	inputBoxEdit.classList.add('show');
-	taskList.style.filter = 'blur(5px)';
+	inputBoxEdit.classList.add("show");
+	taskList.style.filter = "blur(5px)";
 };
 
 const delCheckTask = (e) => {
 	const item = e.target;
-	if (item.classList[1] === 'fa-times') {
+	if (item.classList[1] === "fa-times") {
 		item.parentElement.parentElement.parentElement.remove();
 	}
-	if (item.classList[1] === 'fa-check') {
-		item.parentElement.parentElement.firstChild.classList.toggle('checked');
+	if (item.classList[1] === "fa-check") {
+		item.parentElement.parentElement.firstChild.classList.toggle("checked");
 	}
-	if (item.classList[1] === 'fa-edit') {
+	if (item.classList[1] === "fa-edit") {
 		editTask(e);
 	}
 };
 
 const createTask = () => {
 	// Variables to create list items in Javascript
-	console.log('create-task-hit!');
-	if (taskName.value === '') {
-		alert('Please Enter a Task!');
+	console.log("create-task-hit!");
+	if (taskName.value === "") {
+		alert("Please Enter a Task!");
 		return;
 	}
 
-	const ul = document.querySelector('.task-list');
-	const div = document.createElement('div');
-	const li = document.createElement('li');
-	const divText = document.createElement('div');
-	const divIcons = document.createElement('div');
-	const crossIcon = document.createElement('i');
-	const checkIcon = document.createElement('i');
-	const editIcon = document.createElement('i');
-	const p1 = document.createElement('p');
-	const p2 = document.createElement('p');
+	const ul = document.querySelector(".task-list");
+	const div = document.createElement("div");
+	const li = document.createElement("li");
+	const divText = document.createElement("div");
+	const divIcons = document.createElement("div");
+	const crossIcon = document.createElement("i");
+	const checkIcon = document.createElement("i");
+	const editIcon = document.createElement("i");
+	const p1 = document.createElement("p");
+	const p2 = document.createElement("p");
 
 	p1.innerText = taskName.value;
 	p2.innerText = taskDesc.value;
-	li.style.textTransform = 'capitalize';
-	div.classList.add('task-item');
-	divText.classList.add('task-text');
-	divIcons.classList.add('div-icons');
-	editIcon.classList.add('fas', 'fa-edit', 'edit-icon');
-	crossIcon.classList.add('fas', 'fa-times');
-	checkIcon.classList.add('fas', 'fa-check', 'check');
+	li.style.textTransform = "capitalize";
+	div.classList.add("task-item");
+	divText.classList.add("task-text");
+	divIcons.classList.add("div-icons");
+	editIcon.classList.add("fas", "fa-edit", "edit-icon");
+	crossIcon.classList.add("fas", "fa-times");
+	checkIcon.classList.add("fas", "fa-check", "check");
 
 	divText.appendChild(p1);
 	divText.appendChild(p2);
@@ -198,8 +198,8 @@ const createTask = () => {
 	li.appendChild(divIcons);
 	ul.append(div);
 
-	taskName.value = '';
-	taskDesc.value = '';
+	taskName.value = "";
+	taskDesc.value = "";
 };
 
 toDoApp();
